b512b579d2e3ae4f6fc1775f97b99c2d5aa5acec
AlvaroQQ <alvaro19aef@gmail.com>
2025-11-10 18:19:14 -0400

    primer commit landing page sonidos

---
+++ src/main/java/com/sonidosweb/sonidosweb/SonidoswebApplication.java
@@ -0,0 +1,13 @@
+package com.sonidosweb.sonidosweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonidoswebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonidoswebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sonidosweb/sonidosweb/SonidoswebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonidosweb.sonidosweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonidoswebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
